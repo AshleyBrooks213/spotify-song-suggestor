5db2326459aa0b4a76283b26f1aa77b588c7209c
boscolio <nickrules@gmail.com>
2021-02-03 18:17:02 -0600

    results saved to results.csv

--- app/model.py
+++ app/model.py
@@ -22,7 +22,7 @@ model = NearestNeighbors(n_neighbors=11)
 model.fit(df[df.columns[2:13]])
 
 
-@router.get("/model")
+@router.get('/model')
 async def predict(song='Float On', artist=0):
 
     # Assign input matches to a series
@@ -37,10 +37,13 @@ async def predict(song='Float On', artist=0):
     # Display artist and spotify id of all songs that match the input
     artist_list = df.iloc[obs, 1]
 
-    # Using spotify track id to return dataframe of results
+    # Dataframe of results saved to csv
     df_nn = df.loc[new_obs]
+    df_nn.to_csv(r'app/results.csv', index = False, header = True) 
+
     # Assign songs and artists to a dictionary
     names = df_nn[['name', 'artists']]
     names_dict = names.to_dict(orient='records')
 
     return names_dict, artist_list, df_nn
+
