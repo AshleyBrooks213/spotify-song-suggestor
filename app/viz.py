7853a7189d665478ca51fd9bb2ee9356ac27b518
boscolio <nickrules@gmail.com>
2021-02-04 18:27:54 -0600

    Revised description on main.py

--- app/viz.py
+++ app/viz.py
@@ -3,26 +3,36 @@
 from fastapi import APIRouter, HTTPException
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+
+plot_songs_attributes_boxplots(plotly_df)
 
 router = APIRouter()
-
+df_plot = pd.read_csv('app/results.csv')
 
 @router.get('/viz')
-async def viz():
+async def viz(df=df_plot):
     """
     ### Response
     JSON string to render with [react-plotly.js](https://plotly.com/javascript/react/)
     """
 
-    cols = ['acousticness',	'danceability', 'energy', 'instrumentalness', 'key',
-        'liveness', 'loudness',	'popularity', 'speechiness', 'tempo', 'valence']
+    #cols = ['acousticness',	'danceability', 'energy', 'instrumentalness', 'key',
+    #    'liveness', 'loudness',	'popularity', 'speechiness', 'tempo', 'valence']
+    fig = go.Figure()
+    fig.add_trace(go.Box(x=df.acousticness, name='acousticness'))
+    fig.add_trace(go.Box(x=df.danceability, name = "danceability"))
+    fig.add_trace(go.Box(x=df.energy, name = "energy"))
+    fig.add_trace(go.Box(x=df.instrumentalness, name = "instrumentalness"))
+    fig.add_trace(go.Box(x=df.key, name = "key"))
+    fig.add_trace(go.Box(x=df.liveness, name = "liveness"))
+    fig.add_trace(go.Box(x=df.loudness, name = "loudness"))
+    fig.add_trace(go.Box(x=df.popularity, name = "popularity"))
+    fig.add_trace(go.Box(x=df.speechiness, name = "speechiness"))
+    fig.add_trace(go.Box(x=df.tempo, name = "tempo"))
+    fig.add_trace(go.Box(x=df.valence, name = "valence"))
     
-    df_plot = pd.read_csv('app/results.csv')
-    
-    # Make Plotly figure
-    #fig = px.box(df_plot, orientation='v', column=columns, title='Strength of Song Characteristics')
-    fig = gfp
-    # Plotly figure for song results
+    fig.update_layout(title_text = 'Distribution of Predicted Songs\' Attributes')
 
-    # Return Plotly figure as JSON string
     return fig.to_json()
